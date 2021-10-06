e9a5575e80c0df1862949bd61bc4400a172c4a6b
pareshmaniyar <pareshmaniyar1@gmail.com>
2021-10-06 14:38:44 +0530

    Add deeper prototype example

--- Creational/prototype.ts
+++ Creational/prototype.ts
@@ -7,15 +7,31 @@ abstract class SeedCreator {
 let counter = 0;
 class Mango extends SeedCreator {
     id: number = counter++;
+    rip: boolean = false;
+    weight: number = 0;
+    constructor(weight: number) {
+        super();
+        this.weight = weight;
+    }
     eat() {
         console.log(`Eating mango: ${this.id}`);
     }
+    giveTime() {
+        this.rip = true;
+        console.log("Riped now!");
+    }
     clone() {
-        return new Mango();
+        const clonedObject = new Mango(this.weight);
+        clonedObject.rip = this.rip;
+        return clonedObject;
     }
 }
 
-const mango = new Mango();
+const mango = new Mango(1);
 const mango2 = mango.clone();
-mango2.eat();
+mango.giveTime();
+const mango3 = mango.clone();
 mango.eat();
+mango2.eat();
+mango3.eat();
+console.log(mango, mango2, mango3);
